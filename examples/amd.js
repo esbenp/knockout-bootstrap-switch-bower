92b642fbd3055529d7f6862901a5e18a9923bc6d
Esben Petersen <esbenspetersen@gmail.com>
2015-04-03 01:24:00 +0200

    Wrapped in some UMD and registered in bower

--- examples/amd.js
+++ examples/amd.js
@@ -2,12 +2,20 @@ requirejs.config({
     baseUrl: '../bower_components',
     paths: {
         dist: "../dist",
+        jquery: "jquery/dist/jquery",
+        knockout: "knockout/dist/knockout",
+        "bootstrap-switch": "bootstrap-switch/dist/js/bootstrap-switch",
+        "knockout-bootstrap-switch": "../dist/knockout-bootstrap-switch-bower"
     },
     shim: {
-
+        "bootstrap-switch": ["jquery"],
+        "knockout-bootstrap-switch": ["bootstrap-switch"]
     }
 });
 
-requirejs([], function(){
-    
+requirejs(["knockout", "knockout-bootstrap-switch"], function(ko, ko){
+    var container = document.getElementById("container");
+    ko.applyBindings({
+        state: ko.observable(true)
+    }, container);
 });
